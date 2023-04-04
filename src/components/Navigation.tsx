72647b57c4bb19b160ad4c8cc87596c2b568f384
Natalye-tech <silverpine@mail.ru>
2023-04-04 10:24:04 +0300

    src/components/Navigation.tsx edit

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -7,7 +7,7 @@ export function Navigation() {
       <img width='180px' src='iskra1.svg'></img>
       <span>
         <Link to="/" className="mr-2">page2</Link>
-        <Link to="/page" className="mr-2">About</Link>
+        <Link to="/about" className="mr-2">About</Link>
         <Link to="/page1" className="mr-2">Page1</Link>
       </span>
 
